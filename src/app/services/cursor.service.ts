b95751068fd3187021d7d16c79d804df979cc91c
Keshavamurthy <keshavamurthy.c@version1.com>
2020-06-16 21:38:02 +0530

    minor changes on curosr code

--- src/app/services/cursor.service.ts
+++ src/app/services/cursor.service.ts
@@ -6,15 +6,21 @@ import { Injectable } from '@angular/core';
 export class CursorService {
   top: any;
   left: any;
+  display: string;
   constructor() {}
 
   showCustomCursor(event) {
     //setTimeout(() => {
     this.top = event.clientY - 10 + 'px';
     this.left = event.clientX - 10 + 'px';
+    this.display = 'block';
     // }, 10);
 
     // console.log(event);
+  }
+
+  hideCusromCursor(event) {
+    this.display = 'none';
   }
   // @HostListener('document:mousemove', ['$event'])
   // onMousemove($event) {
