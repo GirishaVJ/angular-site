037c68c33d4616cad6a166e1525e15284b5cbaa6
Keshavamurthy <keshavamurthy.c@version1.com>
2020-06-17 23:43:31 +0530

    added parallax effect

--- src/app/components/hightlights/hightlights.component.ts
+++ src/app/components/hightlights/hightlights.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { CursorService } from '../../services/cursor.service';
 
 @Component({
   selector: 'app-hightlights',
   templateUrl: './hightlights.component.html',
-  styleUrls: ['./hightlights.component.scss']
+  styleUrls: ['./hightlights.component.scss'],
 })
 export class HightlightsComponent implements OnInit {
-
-  constructor() { }
+  constructor(public crsr: CursorService) {}
 
   ngOnInit() {
+    console.log(this.crsr.pos1);
   }
-
 }
--- src/app/services/cursor.service.ts
+++ src/app/services/cursor.service.ts
@@ -7,15 +7,21 @@ export class CursorService {
   top: any;
   left: any;
   display: string;
+  pos1: any;
 
   isHyperlink = false;
-  constructor() {}
+  constructor() {
+    window.addEventListener('scroll', this.scroll, true);
+  }
+
+  scroll = (event): void => {
+    this.pos1 = Number(event.srcElement.scrollingElement.scrollTop * 0.5);
+  };
 
   showCustomCursor(event) {
     this.top = event.clientY - 5 + 'px';
     this.left = event.clientX - 5 + 'px';
     this.display = 'block';
-    // console.log(event);
     if (
       event.target.tagName.toLowerCase() === 'a' ||
       event.target.tagName.toLowerCase() === 'span'
