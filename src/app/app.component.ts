7315531092f4719fad829b2bbbac1a63cacf7c75
Keshavamurthy <keshavamurthy.c@version1.com>
2020-07-18 10:58:11 +0530

    page animation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CursorService } from './services/cursor.service';
+import { fadeAnimation } from './route.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation],
 })
 export class AppComponent implements OnInit {
   title = 'angular';
