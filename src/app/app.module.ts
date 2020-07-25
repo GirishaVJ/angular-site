13a1cc8f410050dc133d23517b093ccf218d7150
Keshavamurthy <keshavamurthy.c@version1.com>
2020-07-26 00:01:52 +0530

    added popup for video player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PennedByOurMindsComponent } from './components/penned-by-our-minds/penn
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { BannerAnimationComponent } from './components/banner-animation/banner-animation.component';
 import { NewsEventsComponent } from './components/news-events/news-events.component';
@@ -62,6 +63,7 @@ import { KnowledgeHubComponent } from './pages/knowledge-hub/knowledge-hub.compo
     BrowserAnimationsModule,
     CarouselModule,
     TabsModule.forRoot(),
+    ModalModule.forRoot(),
   ],
   providers: [CursorService],
   bootstrap: [AppComponent],
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openPopup(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
-
 }
